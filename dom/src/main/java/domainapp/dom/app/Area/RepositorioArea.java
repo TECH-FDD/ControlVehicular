5ac2114e030224ebf0f0b27e22c631135b216267
Delfor <delfor87@gmail.com>
2015-06-04 20:35:22 -0300

    ISSUE #6:Creacion del objeto Area.
    Se cargo la nueva clase con sus respectivos atributos y notaciones.
    Se creo el repositorio Area, con el metodo crearArea, listo para
    persistir.

---
+++ dom/src/main/java/domainapp/dom/app/Area/RepositorioArea.java
@@ -0,0 +1,38 @@
+package domainapp.dom.app.Area;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.query.QueryDefault;
+
+import domainapp.dom.app.Area.Area;
+
+@DomainService(repositoryFor = Area.class)
+@DomainServiceLayout(menuOrder = "30", named="Area")
+public class RepositorioArea {
+	
+	@MemberOrder(sequence = "1")
+    public Area crearArea(
+    	final @ParameterLayout(named="Codigo de Area") String codigo_area,
+        final @ParameterLayout(named="Nombre") String nombre, 
+        final @ParameterLayout(named="Descripcion") String descripcion,
+        final @ParameterLayout(named="Fecha de alta") Timestamp fecha_alta){
+        final Area area = container.newTransientInstance(Area.class);
+        area.setCodigo_area(codigo_area);
+        area.setNombre(nombre);
+        area.setDescripcion(descripcion);
+        area.setFecha_alta(fecha_alta);
+        container.persistIfNotAlready(area);
+        return area;
+
+    	}
+    	
+    	@javax.inject.Inject 
+        DomainObjectContainer container;
+}
