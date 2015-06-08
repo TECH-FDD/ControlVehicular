46fd5ff4a69d2058d33372e2cdb3ca64c6d757c8
Delfor <delfor87@gmail.com>
2015-06-08 18:18:03 -0300

    ISSUE #6: Validacion de campos al crear un Area

--- dom/src/main/java/domainapp/dom/app/Area/RepositorioArea.java
+++ dom/src/main/java/domainapp/dom/app/Area/RepositorioArea.java
@@ -1,16 +1,18 @@
 package domainapp.dom.app.Area;
 
 import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.isis.applib.DomainObjectContainer;
 import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.DomainServiceLayout;
 import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Parameter;
 import org.apache.isis.applib.annotation.ParameterLayout;
 import org.apache.isis.applib.query.QueryDefault;
-import domainapp.dom.app.Area.Area;
 
+import domainapp.dom.app.Area.Area;
 
 @DomainService(repositoryFor = Area.class)
 @DomainServiceLayout(menuOrder = "30", named="Area")
@@ -18,21 +20,25 @@ public class RepositorioArea {
 	
 	@MemberOrder(sequence = "1")
     public Area crearArea(
-    	final @ParameterLayout(named="Codigo de Area") String codigo_area,
-        final @ParameterLayout(named="Nombre") String nombre, 
-        final @ParameterLayout(named="Descripcion") String descripcion,
-        final @ParameterLayout(named="Fecha de alta") Timestamp fecha_alta){
+    	final @ParameterLayout(named="Codigo de Area") @Parameter(regexPattern = domainapp.dom.regex.Validador.Validador.ValidacionAlfanumerico.ADMITIDOS,maxLength=10) String codigo_area,
+        final @ParameterLayout(named="Nombre") @Parameter(regexPattern = domainapp.dom.regex.Validador.Validador.ValidacionAlfanumerico.ADMITIDOS,maxLength=30) String nombre, 
+        final @ParameterLayout(named="Descripcion") String descripcion)
+        /*final @ParameterLayout(named="Fecha de alta") Timestamp fecha_alta)*/{
         final Area area = container.newTransientInstance(Area.class);
+        
+        Date date= new Date();
+        Timestamp fecha = new Timestamp(date.getTime());
+        
         area.setCodigo_area(codigo_area);
         area.setNombre(nombre);
         area.setDescripcion(descripcion);
-        area.setFecha_alta(fecha_alta);
+        area.setFecha_alta(fecha);
         container.persistIfNotAlready(area);
         return area;
 
     	}
-    	
-	@MemberOrder(sequence = "2")
+
+	@MemberOrder(sequence = "2") 
     public List<Area> ListarTodos() {
         return container.allInstances(Area.class);
     }
---
+++ dom/src/main/java/domainapp/dom/regex/Validador/Validador.java
@@ -0,0 +1,12 @@
+package domainapp.dom.regex.Validador;
+
+//import java.util.Calendar;
+
+public class Validador {
+
+	public static final class ValidacionAlfanumerico{ 	
+		private ValidacionAlfanumerico() { }
+		public static final String ADMITIDOS = "[a-z,A-Z,0-9,ñ,Ñ, ]+"; 
+	}
+	
+}
