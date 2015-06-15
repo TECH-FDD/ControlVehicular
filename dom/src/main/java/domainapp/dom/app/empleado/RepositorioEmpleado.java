2b010d1c66cff60cd2ced07fefac8f1555a7e46b
Franco Chiappano <fchiappano@ergio.com.ar>
2015-06-15 19:26:19 -0300

    ISSUE #9: Filtrar busquedas por activo/inactivo.
    Se filtraron todas las busquedas, para que solo se obtengan listas de
    Empleados activos.
    Ademas se agrego una busqueda de Empleados inactivos y se reordenaron
    las acciones en el menu de la aplicación.

--- dom/src/main/java/domainapp/dom/app/empleado/RepositorioEmpleado.java
+++ dom/src/main/java/domainapp/dom/app/empleado/RepositorioEmpleado.java
@@ -1,6 +1,7 @@
 package domainapp.dom.app.empleado;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.isis.applib.DomainObjectContainer;
@@ -75,35 +76,34 @@ public class RepositorioEmpleado {
 		return null;
 	}
 
-	@MemberOrder(sequence = "2")
-    public Empleado BuscarPorDni(
+	@MemberOrder(sequence = "5")
+    public List<Empleado> BuscarPorDocumento(
             @ParameterLayout(named="N° de Documento") @Parameter(regexPattern=domainapp.dom.regex.validador.Validador.ValidacionNumerica.ADMITIDOS)
-            final int nroDocumento
-    ) {
-        return container.firstMatch(new QueryDefault<>(
+            final int nroDocumento) {
+        return filtrarPorActivos(container.allMatches(new QueryDefault<>(
                 Empleado.class,
                 "Buscar_Documento",
-                "nroDocumento", nroDocumento));
-    }
-
-	@MemberOrder(sequence = "4")
-    public List<Empleado> buscarPorNombre(@ParameterLayout(named="Nombre") @Parameter(regexPattern=domainapp.dom.regex.validador.Validador.ValidacionLetras.ADMITIDOS) final String nombre){
-        return container.allMatches(new QueryDefault<>(Empleado.class,"Buscar_Nombre","nombre", nombre));
-    }
-
-	@MemberOrder(sequence = "5")
-    public List<Empleado> buscarPorApellido(@ParameterLayout(named="Apellido") @Parameter(regexPattern=domainapp.dom.regex.validador.Validador.ValidacionLetras.ADMITIDOS) final String apellido){
-        return container.allMatches(new QueryDefault<>(Empleado.class,"Buscar_Apellido","apellido", apellido));
+                "nroDocumento", nroDocumento)));
     }
 
 	@MemberOrder(sequence = "3")
+    public List<Empleado> buscarPorNombre(@ParameterLayout(named="Nombre") @Parameter(regexPattern=domainapp.dom.regex.validador.Validador.ValidacionLetras.ADMITIDOS) final String nombre){
+        return filtrarPorActivos(container.allMatches(new QueryDefault<>(Empleado.class,"Buscar_Nombre","nombre", nombre)));
+    }
+
+	@MemberOrder(sequence = "4")
+    public List<Empleado> buscarPorApellido(@ParameterLayout(named="Apellido") @Parameter(regexPattern=domainapp.dom.regex.validador.Validador.ValidacionLetras.ADMITIDOS) final String apellido){
+        return filtrarPorActivos(container.allMatches(new QueryDefault<>(Empleado.class,"Buscar_Apellido","apellido", apellido)));
+    }
+
+	@MemberOrder(sequence = "2")
     public List<Empleado> listarTodos(){
-        return container.allInstances(Empleado.class);
+        return filtrarPorActivos(container.allInstances(Empleado.class));
     }
 
 	@MemberOrder(sequence = "6")
     public List<Empleado> buscarPorLegajo(@ParameterLayout(named="N° de Legajo")final String legajo){
-        return container.allMatches(new QueryDefault<>(Empleado.class,"Buscar_Legajo","legajo", legajo));
+        return filtrarPorActivos(container.allMatches(new QueryDefault<>(Empleado.class,"Buscar_Legajo","legajo", legajo)));
     }
 
 	public List<Ciudad> choices7CrearEmpleado(String nombre, String apellido, Documento tipoDocumento,
@@ -111,6 +111,31 @@ public class RepositorioEmpleado {
 			 Ciudad ciudad, int codigoPostal,String domicilio, String telefono, String email,String legajo, Area area) {
         return Ciudad.listarPor(provincia);
     }
+
+	@MemberOrder(sequence = "1", name="Elementos Inactivos")
+	public List<Empleado> Empleados(){
+		List<Empleado> lista=container.allInstances(Empleado.class);
+		List<Empleado> inactivos= new ArrayList<Empleado>();
+		for (Empleado empleado : lista){
+			if (!empleado.isActivo())
+				inactivos.add(empleado);
+		}
+		return inactivos;
+	}
+
+	/**
+	 * Filtrar lista por Empleados activos.
+	 * @param  listaEmpleados
+	 * @return Lista de Empleados activos.
+	 */
+	private List<Empleado> filtrarPorActivos(final List<Empleado> lista){
+		List<Empleado> activos= new ArrayList<Empleado>();
+		for (Empleado empleado : lista){
+			if (empleado.isActivo())
+				activos.add(empleado);
+		}
+		return activos;
+	}
 
 	@javax.inject.Inject
     DomainObjectContainer container;
