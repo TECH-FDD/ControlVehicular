bb50518f86319d3a9cb058cd6760ab4ebaf41f7a
Franco Chiappano <fchiappano@ergio.com.ar>
2015-06-19 19:01:26 -0300

    ISSUE #30: Se creo funcionalidad agregar un aceite

--- dom/src/main/java/domainapp/dom/app/aceite/Aceite.java
+++ dom/src/main/java/domainapp/dom/app/aceite/Aceite.java
@@ -1,5 +1,134 @@
 package domainapp.dom.app.aceite;
 
-public class Aceite {
+import java.sql.Timestamp;
 
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.VersionStrategy;
+
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Property;
+import org.apache.isis.applib.annotation.Where;
+
+@javax.jdo.annotations.PersistenceCapable(identityType=IdentityType.DATASTORE)
+@javax.jdo.annotations.DatastoreIdentity(
+        strategy=javax.jdo.annotations.IdGeneratorStrategy.IDENTITY,
+         column="Aceite_ID")
+@javax.jdo.annotations.Version(
+        strategy=VersionStrategy.VERSION_NUMBER,
+        column="version")
+@DomainObject(objectType = "ACEITE")
+@DomainObjectLayout(bookmarking = BookmarkPolicy.AS_CHILD)
+public class Aceite {
+	private String nombre;
+	private String marca;
+	private String codigo;
+	private String descripcion;
+	private TipoAceite tipoAceite;
+	private int duracion;
+	private Timestamp fechaAlta;
+	private boolean activo;
+
+	public Aceite(String nombre, String marca, String codigo,
+			String descripcion, TipoAceite tipoAceite, int duracion, Timestamp fechaAlta, boolean activo) {
+		super();
+		this.nombre = nombre;
+		this.marca = marca;
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.tipoAceite = tipoAceite;
+		this.duracion = duracion;
+		this.fechaAlta= fechaAlta;
+		this.activo=activo;
+	}
+
+	public Aceite() {
+		super();
+	}
+
+	@MemberOrder(sequence="20")
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@MemberOrder(sequence="10")
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	@MemberOrder(sequence="30")
+	@javax.jdo.annotations.Column(allowsNull = "true")
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	@MemberOrder(sequence="40")
+	@javax.jdo.annotations.Column(allowsNull = "true")
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	@MemberOrder(sequence="50")
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	public TipoAceite getTipoAceite() {
+		return tipoAceite;
+	}
+
+	public void setTipoAceite(TipoAceite tipoAceite) {
+		this.tipoAceite = tipoAceite;
+	}
+
+	@MemberOrder(sequence="60")
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	@MemberOrder(sequence="70")
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	public Timestamp getFechaAlta() {
+		return fechaAlta;
+	}
+
+	public void setFechaAlta(Timestamp fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+
+	@Property(hidden=Where.EVERYWHERE)
+	public boolean isActivo() {
+		return activo;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+
+	@Override
+	public String toString() {
+		return marca + " "+ nombre;
+	}
 }
--- dom/src/main/java/domainapp/dom/app/aceite/RepositorioAceite.java
+++ dom/src/main/java/domainapp/dom/app/aceite/RepositorioAceite.java
@@ -1,5 +1,39 @@
 package domainapp.dom.app.aceite;
 
-public class RepositorioAceite {
+import java.sql.Timestamp;
 
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Optionality;
+import org.apache.isis.applib.annotation.Parameter;
+import org.apache.isis.applib.annotation.ParameterLayout;
+
+@DomainService(repositoryFor = Aceite.class)
+@DomainServiceLayout(menuOrder = "60", named="Aceites")
+public class RepositorioAceite {
+	@MemberOrder(sequence = "1")
+	@ActionLayout(named="Crear nuevo Aceite")
+	public Aceite createAceite(
+			@ParameterLayout(named="Marca")String marca,
+			@ParameterLayout(named="Nombre")String nombre,
+			@ParameterLayout(named="Codigo") @Parameter(optionality=Optionality.OPTIONAL)String codigo,
+			@ParameterLayout(named="Descripción")@Parameter(optionality=Optionality.OPTIONAL)String descripcion,
+			@ParameterLayout(named="Tipo de Aceite")TipoAceite tipoAceite,
+			@ParameterLayout(named="Duración (km)")int duracion){
+		final Aceite aceite = container.newTransientInstance(Aceite.class);
+		aceite.setCodigo(codigo);
+		aceite.setDescripcion(descripcion);
+		aceite.setDuracion(duracion);
+		aceite.setFechaAlta(new Timestamp(System.currentTimeMillis()));
+		aceite.setMarca(marca);
+		aceite.setNombre(nombre);
+		aceite.setTipoAceite(tipoAceite);
+		aceite.setActivo(true);
+		return aceite;
+	}
+	@javax.inject.Inject
+    DomainObjectContainer container;
 }
