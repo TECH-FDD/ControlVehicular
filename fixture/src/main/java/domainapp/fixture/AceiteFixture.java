34a9b572dd975a2781ca79196d569c5e5c8ee8c0
Franco Chiappano <fchiappano@ergio.com.ar>
2015-06-30 14:55:16 -0300

    ISSUE #68: Sobrecarga de datos de Aceite.

--- fixture/src/main/java/domainapp/fixture/AceiteFixture.java
+++ fixture/src/main/java/domainapp/fixture/AceiteFixture.java
@@ -1,11 +1,59 @@
 package domainapp.fixture;
 
+import org.apache.isis.applib.DomainObjectContainer;
+
+import domainapp.dom.app.aceite.Aceite;
+import domainapp.dom.app.aceite.RepositorioAceite;
+import domainapp.dom.app.aceite.TipoAceite;
+
 public class AceiteFixture extends Fixture {
+
+	private String nombre="Elaion F10,Helix F10,Lubrax F10";
+	private String marca="Ypf,Shell,Petrobras";
+	private String codigo="YESS,HSSS,PLSS";
+	private String descripcion="Aceite Semi-Sintetico de alta performance.";
+	private int duracion=10000;
+
+	public AceiteFixture() {
+		withDiscoverability(Discoverability.DISCOVERABLE);
+	}
+
+	private String getNombre(int x) {
+		return obtenerValor(nombre, x);
+	}
+
+	private String getMarca(int x) {
+		return obtenerValor(marca, x);
+	}
+
+	private String getCodigo(int x) {
+		return obtenerValor(codigo, x);
+	}
+
+	private int getDuracion() {
+		return duracion;
+	}
+
+	private String getDescripcion() {
+		return descripcion;
+	}
 
 	@Override
 	protected void execute(ExecutionContext executionContext) {
-		// TODO Auto-generated method stub
-
+		borrarTabla(executionContext, "Aceite");
+		for (int x=0; x<3;x++){
+			create(getMarca(x),getNombre(x),getCodigo(x),getDescripcion(),TipoAceite.SemiSintetico,getDuracion(),executionContext);
+		}
 	}
 
+	private Aceite create(final String marca, final String nombre, final String codigo,
+						final String descripcion, final TipoAceite tipoAceite,
+						final int duracion, ExecutionContext executionContext){
+		return executionContext.addResult(this, repoAceite.createAceite(marca, nombre, codigo, descripcion, tipoAceite, duracion));
+	}
+
+	@javax.inject.Inject
+	private RepositorioAceite repoAceite;
+	@javax.inject.Inject
+	DomainObjectContainer container;
 }
