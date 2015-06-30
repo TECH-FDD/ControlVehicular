7cab97207bfea63790bdd1b67d83bf0f5b75858e
Franco Chiappano <fchiappano@ergio.com.ar>
2015-06-30 14:57:50 -0300

    ISSUE #68: Sobrecarga de datos de Combustible.

--- fixture/src/main/java/domainapp/fixture/CombustibleFixture.java
+++ fixture/src/main/java/domainapp/fixture/CombustibleFixture.java
@@ -1,11 +1,80 @@
 package domainapp.fixture;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.apache.isis.applib.DomainObjectContainer;
+
+import domainapp.dom.app.combustible.Combustible;
+import domainapp.dom.app.combustible.RepositorioCombustible;
+import domainapp.dom.app.combustible.TipoCombustible;
+
 public class CombustibleFixture extends Fixture {
 
-	@Override
-	protected void execute(ExecutionContext executionContext) {
-		// TODO Auto-generated method stub
+	private static String nombre="V-Power,Super,Infinia,Super,Premium,Super";
+	private static String empresa="Shell,Shell,Ypf,Ypf,Petrobras,Petrobras";
+	private static String descripcion="Combustible de exelente calidad.";
+	private static String codigo="VPS,SS,IY,SY,PP,SP";
+	private static String categoria="Premium,Super,Premium,Super,Premium,Super";
+	private static String precioLitro="13.5,12.5,13.45,12.45,13.55,12.65";
+	private static String precioAnterior="12.95,11.95,12.90,11.90,12.00,11.00";
+	private static String porcentajeAumento="1.5,0.5,1.2,0.6,0.9,1.3";
+	private static String octanaje="105,98,102,95,103,96";
 
+	private String getNombre(int x){
+		return obtenerValor(nombre, x);
 	}
 
+	private String getEmpresa(int x){
+		return obtenerValor(empresa,x);
+	}
+
+	private String getCodigo(int x){
+		return obtenerValor(codigo, x);
+	}
+
+	private String getCategoria(int x){
+		return obtenerValor(categoria, x);
+	}
+
+	private BigDecimal getPrecioLitro(int x){
+		BigDecimal bd= new BigDecimal(obtenerValor(precioLitro, x));
+		return bd;
+	}
+
+	private BigDecimal getPrecioAterior(int x){
+		BigDecimal bd= new BigDecimal(obtenerValor(precioAnterior, x));
+		return bd;
+	}
+
+	private BigDecimal getPorcentajeAumento(int x){
+		BigDecimal bd= new BigDecimal(obtenerValor(porcentajeAumento, x));
+		return bd;
+	}
+
+	private int getOctanaje(int x){
+		return Integer.parseInt(obtenerValor(octanaje, x));
+	}
+
+	public CombustibleFixture() {
+		withDiscoverability(Discoverability.DISCOVERABLE);
+	}
+	
+	@Override
+	protected void execute(ExecutionContext executionContext) {
+		borrarTabla(executionContext, "Combustible");
+		List<TipoCombustible> tiposCombustible= container.allInstances(TipoCombustible.class);
+		for (int x=0; x<6; x++){
+			create(getNombre(x),getEmpresa(x),getCodigo(x),getCategoria(x),getPrecioLitro(x),getPrecioAterior(x),getPorcentajeAumento(x),getOctanaje(x),tiposCombustible.get(0),executionContext);
+		}
+	}
+
+	private Combustible create(String nombre,String empresa,String codigo,String categoria,BigDecimal precioLitro,BigDecimal precioAnterior,BigDecimal porcentajeAumento, int octanaje,TipoCombustible tipoCombustible,ExecutionContext executionContext ){
+		return executionContext.addResult(this, repoCombustible.createCombustible(nombre, empresa, codigo, descripcion, categoria, precioLitro, precioAnterior, porcentajeAumento, octanaje, tipoCombustible));
+	}
+
+	@javax.inject.Inject
+	private RepositorioCombustible repoCombustible;
+	@javax.inject.Inject
+	DomainObjectContainer container;
 }
