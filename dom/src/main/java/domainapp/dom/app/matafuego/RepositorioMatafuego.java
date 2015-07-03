b17f3e25e874cbf9557a440948caaee44fa00caf
Delfor <delfor87@gmail.com>
2015-07-02 21:40:32 -0300

    ISSUE #16: Correccion de atributos incorrectos

--- dom/src/main/java/domainapp/dom/app/matafuego/RepositorioMatafuego.java
+++ dom/src/main/java/domainapp/dom/app/matafuego/RepositorioMatafuego.java
@@ -1,4 +1,5 @@
 package domainapp.dom.app.matafuego;
+
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -11,25 +12,24 @@ import org.apache.isis.applib.annotation.Parameter;
 import org.apache.isis.applib.annotation.ParameterLayout;
 import org.apache.isis.applib.query.QueryDefault;
 
+import domainapp.dom.app.vehiculo.Vehiculo;
 
-@DomainService(repositoryFor=Matafuego.class)
-@DomainServiceLayout(menuOrder="80",named="Matafuego")
+@DomainService(repositoryFor = Matafuego.class)
+@DomainServiceLayout(menuOrder = "80", named = "Matafuego")
 public class RepositorioMatafuego {
-	
-	@MemberOrder(sequence="1")
-	@ActionLayout(named="Crear matafuego")
+
+	@MemberOrder(sequence = "1")
+	@ActionLayout(named = "Crear matafuego")
 	public Matafuego createMatafuego(
-		final @ParameterLayout(named="Nombre") @Parameter(regexPattern=domainapp.dom.regex.validador.Validador.ValidacionAlfanumerico.ADMITIDOS) String nombre,
-		final @ParameterLayout(named="codigo") @Parameter(regexPattern=domainapp.dom.regex.validador.Validador.ValidacionAlfanumerico.ADMITIDOS) String codigo,
-		final @ParameterLayout(named="Descripcion") @Parameter(regexPattern=domainapp.dom.regex.validador.Validador.ValidacionAlfanumerico.ADMITIDOS) String descripcion,
-		final @ParameterLayout(named="Capacidad") @Parameter(regexPattern=domainapp.dom.regex.validador.Validador.ValidacionNumerica.ADMITIDOS) int capacidad,
-		final @ParameterLayout(named="Fecha de alta") Timestamp fechaAlta,
-		final @ParameterLayout(named="Fecha de recarga") Timestamp fechaRecarga,
-		final @ParameterLayout(named="Fecha de caducidad") Timestamp fechaCadRecarga,
-		final @ParameterLayout(named="Vehiculo") @Parameter(regexPattern=domainapp.dom.regex.validador.Validador.ValidacionAlfanumerico.ADMITIDOS) String vehiculo,
-		final @ParameterLayout(named="Mantenimiento") @Parameter(regexPattern=domainapp.dom.regex.validador.Validador.ValidacionAlfanumerico.ADMITIDOS) String mantenimiento,
-		final @ParameterLayout(named="Reparacion") @Parameter(regexPattern=domainapp.dom.regex.validador.Validador.ValidacionAlfanumerico.ADMITIDOS) String reparacion){
-		
+			final @ParameterLayout(named = "Nombre") @Parameter(regexPattern = domainapp.dom.regex.validador.Validador.ValidacionAlfanumerico.ADMITIDOS) String nombre,
+			final @ParameterLayout(named = "codigo") @Parameter(regexPattern = domainapp.dom.regex.validador.Validador.ValidacionAlfanumerico.ADMITIDOS) String codigo,
+			final @ParameterLayout(named = "Descripcion") @Parameter(regexPattern = domainapp.dom.regex.validador.Validador.ValidacionAlfanumerico.ADMITIDOS) String descripcion,
+			final @ParameterLayout(named = "Capacidad") @Parameter(regexPattern = domainapp.dom.regex.validador.Validador.ValidacionNumerica.ADMITIDOS) int capacidad,
+			final @ParameterLayout(named = "Fecha de alta") Timestamp fechaAlta,
+			final @ParameterLayout(named = "Fecha de recarga") Timestamp fechaRecarga,
+			final @ParameterLayout(named = "Fecha de caducidad") Timestamp fechaCadRecarga,
+			final @ParameterLayout(named = "Vehiculo") @Parameter(regexPattern = domainapp.dom.regex.validador.Validador.ValidacionAlfanumerico.ADMITIDOS) Vehiculo vehiculo) {
+
 		Matafuego matafuego = container.newTransientInstance(Matafuego.class);
 		matafuego.setNombre(nombre);
 		matafuego.setCodigo(codigo);
@@ -39,21 +39,22 @@ public class RepositorioMatafuego {
 		matafuego.setFechaRecarga(fechaRecarga);
 		matafuego.setFechaCadRecarga(fechaCadRecarga);
 		matafuego.setVehiculo(vehiculo);
-		matafuego.setMantenimiento(mantenimiento);
-		matafuego.setReparacion(reparacion);
 		container.persistIfNotAlready(matafuego);
-		return matafuego; 
-	}  
-	@MemberOrder(sequence="2")
-	@ActionLayout(named="Listar todos")
-	public List<Matafuego> listAll(){
-		List<Matafuego> lista = this.container.allMatches(new QueryDefault<Matafuego>(Matafuego.class,
-				"ListarTodos"));
-		if(lista.isEmpty()){
+		return matafuego;
+	}
+
+	@MemberOrder(sequence = "2")
+	@ActionLayout(named = "Listar todos")
+	public List<Matafuego> listAll() {
+		List<Matafuego> lista = this.container
+				.allMatches(new QueryDefault<Matafuego>(Matafuego.class,
+						"ListarTodos"));
+		if (lista.isEmpty()) {
 			this.container.warnUser("No hay areas cargadas en el sistema");
 		}
 		return lista;
 	}
+
 	@javax.inject.Inject
 	DomainObjectContainer container;
 }
