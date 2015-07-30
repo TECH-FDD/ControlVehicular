269f7d2b17309f0b0c0f321448a55dc5368e8407
Franco Chiappano <fchiappano@ergio.com.ar>
2015-07-30 00:05:32 -0300

    ISSUE #71: Desactivar un Vehiculo.

--- dom/src/main/java/domainapp/dom/app/vehiculo/Vehiculo.java
+++ dom/src/main/java/domainapp/dom/app/vehiculo/Vehiculo.java
@@ -1,22 +1,27 @@
 package domainapp.dom.app.vehiculo;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.jdo.annotations.Column;
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.VersionStrategy;
 
+import org.apache.isis.applib.DomainObjectContainer;
 import org.apache.isis.applib.annotation.BookmarkPolicy;
 import org.apache.isis.applib.annotation.DomainObject;
 import org.apache.isis.applib.annotation.DomainObjectLayout;
 import org.apache.isis.applib.annotation.Editing;
 import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.annotation.Property;
 
 import domainapp.dom.app.estadoelemento.Activo;
 import domainapp.dom.app.estadoelemento.Estado;
 import domainapp.dom.app.estadoelemento.Motivo;
+import domainapp.dom.app.estadoelemento.ServicioEstado;
 import domainapp.dom.app.gps.Gps;
 import domainapp.dom.app.matafuego.Matafuego;
 import domainapp.dom.app.aceite.Aceite;
@@ -61,6 +66,7 @@ public class Vehiculo {
 	private String cnsCombuestibleCiudad;
 	private String kilometros;
 	private Estado estado;
+	private ServicioEstado servicioEstado;
 
 	@Persistent
 	@MemberOrder(sequence = "1")
@@ -248,6 +254,15 @@ public class Vehiculo {
 		this.estado = estado;
 	}
 
+	@Programmatic
+	public ServicioEstado getServicioEstado() {
+		return servicioEstado;
+	}
+
+	public void setServicioEstado(ServicioEstado servicioEstado) {
+		this.servicioEstado = servicioEstado;
+	}
+
 	@Override
 	public String toString() {
 		return marca + ", " + modelo;
@@ -281,4 +296,54 @@ public class Vehiculo {
 		super();
 	}
 
+	/**
+	 * Desactivar un Vehiculo, para que el mismo no pueda usarse en un vehiculo.
+	 *
+	 * @return Matafuego con estado Actualizado.
+	 */
+	public Vehiculo desactivar(@ParameterLayout(named="Motivo") Motivo motivo){
+		//Obtengo el nuevo Estado.
+		this.setServicioEstado(this.getServicioEstado().obtenerServicio(this.getEstado()));
+		Estado e= this.getServicioEstado().desactivar(new Timestamp(System.currentTimeMillis()), motivo);
+		//Si el nuevo estado es nulo, quiere decir que no se puede cambiar de estado.
+		if (e==null){
+			container.informUser("Por algúna razón, el Vehiculo seleccionado, ya se encuentra Inactivo. "
+					+ "Por favor, revisar el listado de Elementos Inactivos del Sistema.");
+			return this;
+		}
+
+		//Guardo el anterior estado temporalmente, para eliminarlo de Base de Datos.
+		Estado old= this.getEstado();
+		this.setEstado(e);
+
+		//Actualizo el Matafuego con el nuevo estado.
+		container.persistIfNotAlready(this);
+
+		//Elimino el estado anterior.
+		container.removeIfNotAlready(old);
+
+		container.informUser("El Vehiculo, ha sido desactivado con exito.");
+		return this;
+	}
+
+	/**
+	 * Validar la lista de motivos a mostrar al momento de desactivar un Vehiculo.
+	 * @param motivo
+	 * @return lista de motivos.
+	 */
+	public List<Motivo> choices0Desactivar(Motivo motivo){
+		return Motivo.listar("desactivar");
+	}
+
+	/**
+	 * Verificar si se debe mostrar el boton Desactivar.
+	 *
+	 * @return Confirmacion
+	 */
+	public boolean hideDesactivar(){
+		return servicioEstado.ocultarDesactivar(this.getEstado());
+	}
+
+	@javax.inject.Inject
+	DomainObjectContainer container;
 }
