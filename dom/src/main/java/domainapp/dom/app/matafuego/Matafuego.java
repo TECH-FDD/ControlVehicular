6d105109c64c2c2dcfa7c88e9ae0b8255aac0212
Franco Chiappano <fchiappano@ergio.com.ar>
2015-08-06 23:28:35 -0300

    ISSUE 69: Reactivar un Matafuego.

--- dom/src/main/java/domainapp/dom/app/matafuego/Matafuego.java
+++ dom/src/main/java/domainapp/dom/app/matafuego/Matafuego.java
@@ -15,14 +15,13 @@ import org.apache.isis.applib.annotation.DomainObjectLayout;
 import org.apache.isis.applib.annotation.Editing;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.ParameterLayout;
-import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.annotation.Property;
 
 import domainapp.dom.app.estadoelemento.Activo;
 import domainapp.dom.app.estadoelemento.Asignado;
 import domainapp.dom.app.estadoelemento.Estado;
+import domainapp.dom.app.estadoelemento.Inactivo;
 import domainapp.dom.app.estadoelemento.Motivo;
-import domainapp.dom.app.estadoelemento.ServicioEstado;
 
 @javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
 @javax.jdo.annotations.DatastoreIdentity(strategy = javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column = "Matafuego_ID")
@@ -43,7 +42,6 @@ public class Matafuego {
 	private Timestamp fechaRecarga;
 	private Timestamp fechaCadRecarga;
 	private Estado estado;
-	private ServicioEstado servicioEstado;
 
 	public Matafuego(String marca, String codigo, String descripcion,
 			int capacidad, Timestamp fechaAlta, Timestamp fechaRecarga,
@@ -61,7 +59,6 @@ public class Matafuego {
 
 	public Matafuego() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	@Persistent
@@ -157,15 +154,6 @@ public class Matafuego {
 		this.estado = estado;
 	}
 
-	@Programmatic
-	public ServicioEstado getServicioEstado() {
-		return servicioEstado;
-	}
-
-	public void setServicioEstado(ServicioEstado servicioEstado) {
-		this.servicioEstado = servicioEstado;
-	}
-
 	@Override
 	public String toString() {
 		return "Matafuego " + marca;
@@ -208,16 +196,8 @@ public class Matafuego {
 	 *
 	 * @return this
 	 */
-	public Matafuego activar(){
-		this.setServicioEstado(servicioEstado.obtenerServicio(this.getEstado()));
-		Object[] o = servicioEstado.activar(new Timestamp(System.currentTimeMillis()),null);
-		if (o[0] != null){
-			Estado oldEstado = this.getEstado();
-			this.setEstado((Estado) o[0]);
-			container.persistIfNotAlready(this);
-			container.removeIfNotAlready(oldEstado);
-		}
-		container.warnUser((String) o[1]);
+	public Matafuego reactivar(){
+		this.getEstado().reactivarMatafuego(this);
 		return this;
 	}
 
@@ -226,8 +206,11 @@ public class Matafuego {
 	 *
 	 * @return Confirmacion de si se debe mostrar el Boton.
 	 */
-	public boolean hideActivar(){
-		return this.servicioEstado.ocultarActivar(this.getEstado());
+	public boolean hideReactivar(){
+		if (this.getEstado() instanceof Inactivo)
+			return false;
+		else
+			return true;
 	}
 
 	@javax.inject.Inject
