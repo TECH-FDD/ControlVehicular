49a951b7de33be4b27e28856bfefed97d10d6be7
aburgardt <ale_negryta@hotmail.com.ar>
2015-08-19 19:51:58 -0300

    ISSUE #48: Agregar una alarma Matafuego

---
+++ dom/src/main/java/domainapp/dom/app/alerta/AlertaMatafuego.java
@@ -0,0 +1,81 @@
+package domainapp.dom.app.alerta;
+
+import java.util.Date;
+
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.VersionStrategy;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.Editing;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Optionality;
+import org.apache.isis.applib.annotation.Parameter;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.Property;
+import org.apache.isis.applib.annotation.ActionLayout.Position;
+
+import domainapp.dom.app.empleado.Empleado;
+import domainapp.dom.app.matafuego.Matafuego;
+
+@javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
+@javax.jdo.annotations.DatastoreIdentity(strategy = javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column = "AlertaMatafuego_ID")
+@javax.jdo.annotations.Version(strategy = VersionStrategy.VERSION_NUMBER, column = "version")
+
+@DomainObject(objectType = "ALERTAMATAFUEGO")
+@DomainObjectLayout(bookmarking = BookmarkPolicy.AS_CHILD)
+public class AlertaMatafuego extends Alerta {
+
+	private Matafuego matafuego;
+	private Date contadorAlerta;
+
+	@Persistent
+	@MemberOrder(sequence = "21")
+	@javax.jdo.annotations.Column(allowsNull = "Matafuego")
+	@Property(editing = Editing.DISABLED)
+	public Matafuego getMatafuego() {
+		return matafuego;
+	}
+
+	public void setMatafuego(Matafuego matafuego) {
+		this.matafuego = matafuego;
+	}
+
+	@Persistent
+	@MemberOrder(sequence = "22")
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	@Property(editing = Editing.DISABLED)
+	public Date getContadorAlerta() {
+		return contadorAlerta;
+	}
+
+	public void setContadorAlerta(Date contadorAlerta) {
+		this.contadorAlerta = contadorAlerta;
+	}
+
+	@Override
+	public String toString() {
+		return "AlertaMatafuego: " + getMatafuego().toString();
+	}
+
+	public AlertaMatafuego() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AlertaMatafuego(String nombre, String descripcion, Date fechaAlta,
+			Empleado empleado, Matafuego matafuego, Date contadorAlerta,
+			Date fechaModificacion, Empleado empleadoModificacion) {
+		super(nombre, descripcion, fechaAlta, empleado);
+		this.matafuego = matafuego;
+		this.contadorAlerta = contadorAlerta;
+	}
+
+	@javax.inject.Inject
+	DomainObjectContainer container;
+
+}
---
+++ dom/src/main/java/domainapp/dom/app/alerta/RepositorioAlertaMatafuego.java
@@ -0,0 +1,59 @@
+package domainapp.dom.app.alerta;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Optionality;
+import org.apache.isis.applib.annotation.Parameter;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.query.QueryDefault;
+
+import domainapp.dom.app.empleado.Empleado;
+import domainapp.dom.app.matafuego.Matafuego;
+
+@DomainService(repositoryFor = AlertaMatafuego.class)
+@DomainServiceLayout(menuOrder = "120", named = "Alerta")
+public class RepositorioAlertaMatafuego {
+	@MemberOrder(sequence = "1")
+	@ActionLayout(named = "Crear Nueva Alerta Matafuego")
+	public AlertaMatafuego createAlertaMatafuego(
+			
+			final @ParameterLayout(named = "Nombre") @Parameter(regexPattern = domainapp.dom.regex.validador.Validador.ValidacionAlfanumerico.ADMITIDOS) String nombre,
+			final @ParameterLayout(named = "Descripcion") @Parameter(regexPattern = domainapp.dom.regex.validador.Validador.ValidacionAlfanumerico.ADMITIDOS,optionality = Optionality.OPTIONAL) String descripcion,
+			final @ParameterLayout(named = "Creado Por: ") Empleado empleado,
+			final @ParameterLayout(named = "Matafuego")Matafuego matafuego,
+			final @ParameterLayout(named="Fecha Alerta")Date contadorAlerta)
+			 {
+
+		final AlertaMatafuego alertaMatafuego = container
+				.newTransientInstance(AlertaMatafuego.class);
+		alertaMatafuego.setNombre(nombre);
+		alertaMatafuego.setDescripcion(descripcion);
+		alertaMatafuego.setEmpleado(empleado);
+		alertaMatafuego.setMatafuego(matafuego);
+		alertaMatafuego.setFechaAlta(new Date(System.currentTimeMillis()));
+		alertaMatafuego.setContadorAlerta(contadorAlerta);
+		
+		
+		container.persistIfNotAlready(alertaMatafuego);
+		return alertaMatafuego;
+	}
+
+	// Validar contadorAlerta
+	public String validateCreateAlertaMatafuego(String nombre, String detalle,
+			Empleado empleado,Matafuego matafuego,Date contadorAlerta) {
+
+			if (contadorAlerta.before(new Date(System.currentTimeMillis())))
+				return "La fecha de la Alerta ingresado, debe ser posterior a la fecha actual";
+	
+		return null;
+	}
+
+	@javax.inject.Inject
+	DomainObjectContainer container;
+}
