a1ab77e418a08769b9f2cac1e4f15a54f957017c
aburgardt <ale_negryta@hotmail.com.ar>
2015-09-25 13:29:40 -0300

    ISSUE #62: Creacion de los Estados Alerta

---
+++ dom/src/main/java/domainapp/dom/app/Estadoalerta/AlertaAmarilla.java
@@ -0,0 +1,70 @@
+package domainapp.dom.app.Estadoalerta;
+
+import java.sql.Timestamp;
+
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.VersionStrategy;
+
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.Programmatic;
+
+import domainapp.dom.app.alerta.AlertaMatafuego;
+import domainapp.dom.app.alerta.AlertaVehiculo;
+
+@javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
+@javax.jdo.annotations.DatastoreIdentity(strategy = javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column = "AlertaAmarilla_ID")
+@javax.jdo.annotations.Version(strategy = VersionStrategy.VERSION_NUMBER, column = "version")
+
+@DomainObject(objectType = "ALERTAAMARILLA")
+
+@DomainObjectLayout(bookmarking = BookmarkPolicy.AS_CHILD)
+public class AlertaAmarilla extends EstadoAlerta {
+
+	public AlertaAmarilla() {
+		super();
+	}
+
+	public AlertaAmarilla(Timestamp fechaCambio) {
+		super(fechaCambio);
+	}
+
+	@Override
+	public String toString() {
+		return "Alerta Amarilla";
+	}
+
+	@Override
+	@Programmatic
+	public void cambiarAlerta(AlertaMatafuego matafuego) {
+		EstadoAlerta estado = new AlertaNaranja(new Timestamp(System.currentTimeMillis()));
+		actualizarAlertaMatafuego(matafuego, estado);
+	}
+
+	@Override
+	@Programmatic
+	public void cambiarAlerta(AlertaVehiculo vehiculo) {
+		EstadoAlerta estado = new AlertaNaranja(new Timestamp(System.currentTimeMillis()));
+		actualizarAlertaVehiculo(vehiculo, estado);
+
+	}
+
+	@Override
+	@Programmatic
+	public void aplazarAlertas(AlertaMatafuego matafuego) {
+		EstadoAlerta estado = new Aplazado(new Timestamp(System.currentTimeMillis()));
+		actualizarAlertaMatafuego(matafuego, estado);
+		container.informUser("Se aplazo Exitosamente.");
+
+	}
+
+	@Override
+	@Programmatic
+	public void aplazarAlertas(AlertaVehiculo vehiculo) {
+		EstadoAlerta estado = new Aplazado(new Timestamp(System.currentTimeMillis()));
+		actualizarAlertaVehiculo(vehiculo, estado);
+		container.informUser("Se aplazo Exitosamente.");
+
+	}
+}
---
+++ dom/src/main/java/domainapp/dom/app/Estadoalerta/EstadoAlerta.java
@@ -0,0 +1,101 @@
+package domainapp.dom.app.Estadoalerta;
+
+import java.sql.Timestamp;
+
+import javax.jdo.annotations.Discriminator;
+import javax.jdo.annotations.DiscriminatorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.Editing;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Property;
+
+import domainapp.dom.app.alerta.AlertaMatafuego;
+import domainapp.dom.app.alerta.AlertaVehiculo;
+
+@PersistenceCapable
+@Discriminator(strategy = DiscriminatorStrategy.CLASS_NAME)
+public abstract class EstadoAlerta {
+	private Timestamp fechaCambio;
+
+	public EstadoAlerta(Timestamp fechaCambio) {
+		super();
+		this.fechaCambio = fechaCambio;
+	}
+
+	public EstadoAlerta() {
+		super();
+	}
+
+	@Persistent
+	@MemberOrder(sequence = "1")
+	@javax.jdo.annotations.Column(allowsNull = "true")
+	@Property(editing = Editing.DISABLED)
+	public Timestamp getFechaCambio() {
+		return fechaCambio;
+	}
+
+	public void setFechaCambio(Timestamp fechaCambio) {
+		this.fechaCambio = fechaCambio;
+	}
+
+	@Override
+	public String toString() {
+		return "";
+	}
+
+	/*********************************
+	 * Cambiar alerta.*
+	 *********************************/
+	public abstract void cambiarAlerta(AlertaMatafuego matafuego);
+
+	public abstract void cambiarAlerta(AlertaVehiculo vehiculo);
+
+	/**********************************
+	 * Postergar Alertas*
+	 **********************************/
+
+	public abstract void aplazarAlertas(AlertaMatafuego matafuego);
+
+	public abstract void aplazarAlertas(AlertaVehiculo vehiculo);
+
+	/**
+	 * Actualizar la alerta matafuergo con el nuevo estado y eliminar el estado
+	 * anterior de la BD.
+	 * 
+	 * @param alertaMatafuego
+	 * @param estadoAlerta
+	 */
+	protected void actualizarAlertaMatafuego(AlertaMatafuego alerta, EstadoAlerta estado) {
+		// Obtengo el estado anterior del Gps
+		EstadoAlerta old = alerta.getEstadoAlerta();
+		// Seteo el nuevo estado.
+		alerta.setEstadoAnterior(old.toString());
+		alerta.setEstadoAlerta(estado);
+		container.persistIfNotAlready(alerta);
+		container.removeIfNotAlready(old);
+	}
+
+	/**
+	 * Actualizar la alerta vehiculo con el nuevo estado y eliminar el estado
+	 * anterior de la BD.
+	 * 
+	 * @param alertaVehiculo
+	 * @param estadoAlerta
+	 */
+	protected void actualizarAlertaVehiculo(AlertaVehiculo alerta, EstadoAlerta estado) {
+		// Obtengo el estado anterior del Gps
+		EstadoAlerta old = alerta.getEstadoAlerta();
+		// Seteo el nuevo estado.
+		alerta.setEstadoAlerta(estado);
+		alerta.setEstadoAnterior(old.toString());
+		container.persistIfNotAlready(alerta);
+		container.removeIfNotAlready(old);
+
+	}
+
+	@javax.inject.Inject
+	DomainObjectContainer container;
+}
