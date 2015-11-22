aacaf3711ae4998f0d05c35fef1ca061c1fd0a20
Franco Chiappano <fchiappano@ergio.com.ar>
2015-11-22 00:00:55 -0300

    ISSUE #1: Se elimino toda ref. a Simple Object.

--- webapp/src/main/java/domainapp/webapp/SimpleApplication.java
+++ webapp/src/main/java/domainapp/webapp/SimpleApplication.java
@@ -124,11 +124,11 @@ public class SimpleApplication extends IsisWicketApplication {
         final Module overrides = new AbstractModule() {
             @Override
             protected void configure() {
-                bind(String.class).annotatedWith(Names.named("applicationName")).toInstance("Simple App");
+                bind(String.class).annotatedWith(Names.named("applicationName")).toInstance("S.I.G.A.F.V.");
                 bind(String.class).annotatedWith(Names.named("applicationCss")).toInstance("css/application.css");
                 bind(String.class).annotatedWith(Names.named("applicationJs")).toInstance("scripts/application.js");
                 bind(String.class).annotatedWith(Names.named("welcomeMessage")).toInstance(readLines(getClass(), "welcome.html"));
-                bind(String.class).annotatedWith(Names.named("aboutMessage")).toInstance("Simple App");
+                bind(String.class).annotatedWith(Names.named("aboutMessage")).toInstance("S.I.G.A.F.V.");
                 bind(InputStream.class).annotatedWith(Names.named("metaInfManifest")).toProvider(Providers.of(getServletContext().getResourceAsStream("/META-INF/MANIFEST.MF")));
             }
         };
